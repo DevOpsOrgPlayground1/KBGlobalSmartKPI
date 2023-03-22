1322359f5f9e08d2dc35afe2a33828748b3182b3
nitin-sriv <117272536+nitin-sriv@users.noreply.github.com>
2023-03-23 00:42:05 +0530

    Update azure_dbTesting.ps1

--- azure_dbTesting.ps1
+++ azure_dbTesting.ps1
@@ -2,6 +2,7 @@ $User = $args[0]
 $PW = $args[1]
 $server = $args[2]
 $DB = $args[3]
+$File = $args[4]
 
 echo '********************************************************'
 echo 'User:'
@@ -14,7 +15,7 @@ echo '********************************************************'
 
 echo 'start'
 sqlcmd -S $server -U $User -P $PW -d $DB -Q "select getdate()"
-sqlcmd -S $server -U $User -P $PW -d $DB -i "EXEC tSQLt.RunAll"
+sqlcmd -S $server -U $User -P $PW -d $DB -i $File
 sqlcmd -S $server -U $User -P $PW -d $DB -Q "select getdate()"
 echo 'end'
 echo '********************************************************'
